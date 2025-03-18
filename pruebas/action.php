21279006f000be7dab025cb62880eff0df630b87
Francisco Javier Fernández Rumí <159903784+JaviRomano@users.noreply.github.com>
2025-03-18 21:53:02 +0100

    Add files via upload

---
+++ pruebas/action.php
@@ -0,0 +1,42 @@
+<?php
+$title = "Datos de Formulario";
+include 'header.php';
+
+
+$name = htmlspecialchars($_POST["name"]);
+$phone = htmlspecialchars($_POST["phone"]);
+$email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+$birthdate = $_POST["birthdate"];
+$department = htmlspecialchars($_POST["department"]);
+
+$birthdate_cal = new DateTime($birthdate);
+$today = new DateTime();
+$age = $today->diff($birthdate_cal)->y;
+
+$departments = [
+    "hhrr" => "recursos humanos",
+    "service" => "servicios",
+    "sells" => "ventas",
+    "another" => "otro"
+];
+$department_text = $departments[$department];
+
+
+echo "<div class='container mt-5'>";
+echo "<h2 class='text-black bg-sky text-center d-inline-block border rounded p-3'>Datos Recibidos</h2>";
+echo "<p><strong>Nombre:</strong> $name</p>";
+echo "<p><strong>Teléfono:</strong> $phone</p>";
+echo "<p><strong>Email:</strong> $email</p>";
+echo "<p><strong>Fecha de Nacimiento:</strong> $birthdate</p>";
+echo "<p><strong>Edad:</strong> $age años</p>";
+
+echo "<div class='bg-sky d-inline-block border rounded p-3'>";
+echo "<p> Hola, $name.</p>";
+echo "Confirmamos que tenemos sus datos de contacto.";
+echo "<br>";
+echo "La persona del area de <strong>$department_text</strong> se pondra en contacto contigo <br> ";
+echo "</div>";
+echo "<br>";
+
+echo "<a href='contact.php' class='btn btn-sky mt-3'>Volver</a>";
+echo "</div>";
---
+++ pruebas/contact.php
@@ -0,0 +1,59 @@
+<?php
+$title = "Contacto";
+include 'header.php';
+
+?>
+
+
+<br>
+<p class="text-center">Si necesita ponerse en contacto no dude en rellenar el siguiente formulario y nos pondremos en contacto con usted a la mayor brevedad.</p>
+<div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="modalTitle" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header bg-white text-center">
+                <h5 class="text-center">Formulario de contacto</h5>
+            </div>
+            <div class="modal-body mt-4 bg-white">
+                <form action="action.php" method="post" enctype="multipart/form-data" class="p-4 bg-sky border rounded">
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Nombre:</label>
+                        <input type="text" name="name" class="form-control" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="phone" class="form-label">Número de Teléfono:</label>
+                        <input type="tel" name="phone" id="phone" class="form-control" pattern="[0-9]{9,15}" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" name="email" id="email" class="form-control" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="birthdate" class="form-label">Fecha de Nacimiento</label>
+                        <input type="date" name="birthdate" id="birthdate" class="form-control" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="department" class="form-label">¿Con quien desea contactar?</label>
+                        <select name="department" id="department" class="form-select" required>
+                            <option value="" selected disabled>Seleccione departamento al que desea contactar</option>
+                            <option value="hhrr">Recursos Humanos</option>
+                            <option value="service">Servicios</option>
+                            <option value="sells">Ventas</option>
+                            <option value="another">Otro</option>
+                        </select>
+                    </div>
+                    <div class="text-center">
+                        <p><button type="submit" class="btn btn-light">Aceptar</button>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer bg-white">
+                <button type="button" class="btn btn-sky" data-bs-dismiss="modal">Cerrar</button>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="text-center mt-4">
+    <button type="button" class="btn btn-sky" data-bs-toggle="modal" data-bs-target="#myModal">
+        Formulario de contacto
+    </button>
+</div>
